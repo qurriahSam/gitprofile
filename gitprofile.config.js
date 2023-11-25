4359e1f6f70ea6c3b93901b922ac1bdd9f9e7684
qurriahSam <mugirethe1@gmail.com>
2023-11-25 10:03:23 +0300

    style: change theme

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -205,7 +205,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'halloween',
+    defaultTheme: 'retro',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
